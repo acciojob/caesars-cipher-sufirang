5ea7c6817feaafad20f1a2ffac2f94aa4882da12
sufirang <124654106+sufirang@users.noreply.github.com>
2023-04-01 10:57:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Your Script here.
+function rot13(str) {
+  var result = "";
+  for (var i = 0; i < str.length; i++) {
+    var charCode = str.charCodeAt(i);
+    if (charCode >= 65 && charCode <= 90) {
+      // Shift the letter by 13 places
+      charCode = (charCode - 65 + 13) % 26 + 65;
+    }
+    result += String.fromCharCode(charCode);
+  }
+  return result;
+}
